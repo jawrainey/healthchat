16eb90fd3cec8af760aaba9a54e813535a61454e
Jay Rainey <jawrainey@gmail.com>
2015-03-14 17:45:42 +0000

    Added initial message/question models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.socketio import SocketIO
+import os
+
+# The main application folder
+_basedir = os.path.abspath(os.path.dirname('..'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'test.db')
 
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 socketio = SocketIO(app)
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
---
+++ app/models.py
@@ -0,0 +1,39 @@
+from app import db
+
+
+class Message(db.Model):
+    '''
+    Read/Write the content of the message sent/received (type) from system.
+    '''
+    __tablename__ = 'messages'
+
+    # Initially, I was also going to store an ID for each conversation, to allow
+    # analysis of each conversation. That is outside the current project scope.
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    status = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+
+    def __init__(self, _status, _content):
+        self.status = _status  # Used to differentiate between sent/received.
+        self.content = _content
+
+    def __repr__(self):
+        return 'Message & type: %r, -> %r.' % (self.content, self.status)
+
+
+class Question(db.Model):
+    '''
+    Read/Write the question (OEQ, RS) sent to user, including modifying rating.
+    '''
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    question = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, _question, _rating):
+        self.question = _question
+        self.rating = _rating
+
+    def __repr__(self):
+        return 'Question & rating: %r -> %r' % (self.question, self.rating)
