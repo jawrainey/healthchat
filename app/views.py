c72b5baf73a634de5d4ce7573aba63126343ae04
Jay Rainey <jawrainey@gmail.com>
2015-03-15 16:35:37 +0000

    Add initial voting system
    Note: the core select_response algorithm has not been modified yet.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask.ext.socketio import emit
 from messenger import Messenger
-from app import app, socketio
+from app import app, db, models, socketio
 
 
 @app.route('/')
@@ -24,3 +24,32 @@ def user_received_message(user_message):
 def on_connection():
     # Once a connection is established, we want to send the initial message.
     emit('response', {'type': 'service', 'data': Messenger().initial_message()})
+
+
+@socketio.on('vote', namespace='/chat')
+def on_vote(data):
+    # TODO: what about initial OEQ and clarification question?
+    __cast_vote(data['question'], data['rating'])
+    # Only 'update' current question if user is dissatisfied.
+    if 'down' in data['rating']:
+        emit('vote response',
+             {'data': Messenger().open_ended_question(data['prev_user_msg'])})
+
+
+def __cast_vote(_question, _rating):
+    '''
+    Updates the rating of a question voted for by a user.
+
+    Args:
+        question (str): The question to vote against.
+        rating (str): can either be 'up' or 'down'.
+    '''
+    # TODO: what if the question is non-existing?
+    # User modifies client-side. Currently crash. Try/accept required?
+    row = models.Question.query.filter_by(question=_question).first()
+    # The limits exist to not exceed threshold (0, 1)
+    if 'up' in _rating and row.rating <= .9:
+        row.rating += .1
+    if 'down' in _rating and row.rating >= .1:
+        row.rating -= .1
+    db.session.commit()
