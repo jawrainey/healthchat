247895f5aa6ae23763dff2cccfdcf9ee35a80c66
Jay Rainey <jawrainey@gmail.com>
2015-03-19 15:25:30 +0000

    Added general comments to view methods

--- app/views.py
+++ app/views.py
@@ -6,31 +6,49 @@ from app import app, db, models, socketio
 
 @app.route('/')
 def index():
-    # TODO: use bootstrap, and remove js/css to separate files.
+    '''
+    Homepage of website that contains the chat application.
+    '''
     return render_template('index.html')
 
 
 @socketio.on('user', namespace='/chat')
 def user_received_message(user_message):
+    '''
+    Receives a clients message and sends an appropriate contextual response.
+
+    Args:
+        user_message (dict): Contains the user message (data).
+        TODO: simplify argument by sending only a string.
+    '''
     # TODO: currently, empty messages can be sent.
-    # TODO: save message to database for improvements later...
+
     add_new_lines_user_message = user_message['data'].replace('\n', '</br>')
     emit('response', {'type': 'received', 'data': add_new_lines_user_message})
+
     open_ended_question = Messenger().open_ended_question(user_message['data'])
     emit('response', {'type': 'service', 'data': open_ended_question})
 
 
 @socketio.on('connect', namespace='/chat')
 def on_connection():
-    # Once a connection is established, we want to send the initial message.
+    '''
+    Send initial opening message and instructions on connection.
+    '''
     emit('response', {'type': 'service', 'data': Messenger().initial_message()})
 
 
 @socketio.on('vote', namespace='/chat')
 def on_vote(data):
+    '''
+    Improve rating of OEQ when the up/down buttons clicked.
+    Note: a response is only sent IFF the user is dissatisfied (downvotes).
+
+    Args:
+        data (dict): contains users previous message and feedback (rating).
+    '''
     # TODO: what about initial OEQ and clarification question?
     __cast_vote(data['question'], data['rating'])
-    # Only 'update' current question if user is dissatisfied.
     if 'down' in data['rating']:
         emit('vote response',
              {'data': Messenger().open_ended_question(data['prev_user_msg'])})
