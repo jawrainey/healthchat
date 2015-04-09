fb15f75253ef3f919bc3ce51d10591e852dd8d05
Jay Rainey <jawrainey@gmail.com>
2015-04-09 16:07:29 +0100

    Use utilities in manage.py

--- manage.py
+++ manage.py
@@ -29,49 +29,8 @@ def populate_ontology():
     '''
     Add the initial ontology data (from pre-defined data) to the database.
     '''
-    __populate_from_obo()
-
-
-def __populate_from_obo():
-    '''
-    Inserts concepts/terms into the database.
-
-    Moreover, the transitive closure table is also updated upon insertion
-    of an element to ensure it's retrievable later.
-
-    Note: based on - http://blog.adimian.com/2014/10/cte-and-closure-tables/
-    '''
-    from utils import obo
-    # Note: structure.obo MUST contain the initial concepts.
-    path = DevConfig.DATA_FOLER + 'structure.obo'
-    obo_content = [i for i in obo.Parser(path)]
-
-    from app import models
-    known_ids = [tup[0] for tup in db.session.query(models.Nodes.id).all()]
-
-    for i in obo_content:
-        _id = int(str(i.tags['id'][0]).split(':')[1])
-        # The root element does not have a parent.
-        # Assign it a one as PostgreSQL does not accept zero (no parent).
-        _pid = (int(str(i.tags['is_a'][0]).split(':')[1])
-                if 'is_a' in str(i) else 1)
-        _name = str(i.tags['name'][0])
-        # Only add NEW terms to the database.
-        if _id not in known_ids:
-            node = models.Nodes(id=_id, parent=_pid, name=_name)
-            db.session.add(node)
-            db.session.commit()
-            # Add ontological term to node table.
-            # Collect ancestor of parent, and insert into closure table.
-            values = [(i.parent, _id, i.depth + 1)
-                      for i in models.Closure.query.filter_by(child=_pid).all()]
-            for i in values:
-                db.session.add(
-                    models.Closure(parent=i[0], child=i[1], depth=i[2]))
-                db.session.commit()
-            db.session.add(
-                models.Closure(parent=node.id, child=node.id, depth=0))
-            db.session.commit()
+    from utilities import Utils
+    Utils().populate_db_from_obo_file()
 
 
 @manager.command
